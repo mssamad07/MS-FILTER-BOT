9a2826eebe379fe24ec304f20627dc7ae65806e8
mssamad07 <ms.samad.0143@gmail.com>
2024-11-24 21:46:34 +0530

    case information

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @MS-SAMAD
+# Subscribe YouTube Channel For Amazing Bot @MS-SAMAD-07
+# Ask Doubt on telegram @MS_Samad
 
 
 class script(object):
